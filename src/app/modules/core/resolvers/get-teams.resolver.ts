f72f094a8c62c0337276d72fd1b16c75839a9100
peter <peter.berghuis_13@hotmail.com>
2023-03-30 22:42:24 +0200

    fixed resolver

--- src/app/modules/core/resolvers/get-teams.resolver.ts
+++ src/app/modules/core/resolvers/get-teams.resolver.ts
@@ -2,8 +2,21 @@ import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 import { NbaService } from "../../shared/services/nba.service";
 import { Team } from "../../shared/models/team.model";
+import { LoaderService } from "../services/loader.service";
+import { finalize, of } from "rxjs";
 
 export const GetTeamsResolver: ResolveFn<Team[]> =
   (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-    return inject(NbaService).getAllTeams();
+    const loaderService: LoaderService = inject(LoaderService);
+    const nbaService: NbaService = inject(NbaService);
+    loaderService.showLoader();
+
+    if (nbaService.allTeams.length === 0) {
+      return nbaService.getAllTeams().pipe(
+        finalize(() => loaderService.hideLoader())
+      );
+    }
+    return of(nbaService.allTeams).pipe(
+      finalize(() => loaderService.hideLoader())
+    );
   };
